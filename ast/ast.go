b94afc07cecd4cce1073dac155d88bb36b7ab121
Bryan Ramos <bryan.ramos@hey.com>
2022-03-24 06:58:00 -0400

    Add AST node to parse function literals

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 // Node Interface for our AST
@@ -232,4 +233,32 @@ func (bs *BlockStatement) String() string {
 	}
 
 	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	// fn(x,y,z)
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	//fn(params)
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+
 }
