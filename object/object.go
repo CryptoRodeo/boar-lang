cffdd49b6780c574b2b543a6644f58899b82a867
Bryan Ramos <bryan.ramos@hey.com>
2022-04-05 15:11:47 -0400

    tread safely now, we're implementing null

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -38,6 +39,16 @@ func (b *Boolean) Type() ObjectType {
 
 func (b *Boolean) Inspect() string {
 	return fmt.Sprintf("%t", b.Value)
+}
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return "null"
 }
 
 /**
