8beedf1f63a67c7e94703b80cd18ad899e8505f0
Bryan Ramos <bryan.ramos@hey.com>
2022-04-05 15:09:21 -0400

    Adding bools

--- object/object.go
+++ object/object.go
@@ -8,6 +8,7 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
 type Object interface {
@@ -25,6 +26,18 @@ func (i *Integer) Inspect() string {
 
 func (i *Integer) Type() ObjectType {
 	return INTEGER_OBJ
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
 }
 
 /**
