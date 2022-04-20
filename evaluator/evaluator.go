f125f1894dd946eb73e515216c3a2b484937963f
Bryan Ramos <bryan.ramos@hey.com>
2022-04-20 06:58:08 -0400

    WIP: evaluate call expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -79,6 +79,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
 
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
 	case *ast.IfExpression:
 		return evalIfExpression(node, env)
 
@@ -273,6 +283,21 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 	}
 
 	return val
+}
+
+// evaluate expressions (left to right)
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		// if err, stop evaluation, return error
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
 
 /**
