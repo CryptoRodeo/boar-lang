7ccb1f9d8911e0bc0c0f3fdbf37f39b44ca0c7e5
Bryan Ramos <bryan.ramos@hey.com>
2022-04-25 06:40:49 -0400

    add string literal TokenType

--- token/token.go
+++ token/token.go
@@ -10,8 +10,9 @@ const (
 	EOF = "EOF"
 
 	// Idenfifiers + literals
-	IDENT = "IDENT" // add, foobar, x, y, etc.
-	INT   = "INT"   // 123456
+	IDENT  = "IDENT" // add, foobar, x, y, etc.
+	INT    = "INT"   // 123456
+	STRING = "STRING"
 
 	// Operators
 	ASSIGN   = "="
