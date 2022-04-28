0fe88c8f613502760e15208e6e887eddfb2d4d45
Bryan Ramos <bryan.ramos@hey.com>
2022-04-28 06:10:49 -0400

    fully functional len() builtin method

---
+++ evaluator/builtins.go
@@ -0,0 +1,24 @@
+package evaluator
+
+import "monkey/object"
+
+var builtins = map[string]*object.Builtin{
+	//len()
+	"len": &object.Builtin{Fn: __len__},
+}
+
+func __len__(args ...object.Object) object.Object {
+	// len() should only be passed 1 argument
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got %d, wanted 1", len(args))
+	}
+
+	switch arg := args[0].(type) {
+
+	case *object.String:
+		return &object.Integer{Value: int64(len(arg.Value))}
+
+	default:
+		return newError("argument to `len` not supported, got %s", args[0].Type())
+	}
+}
