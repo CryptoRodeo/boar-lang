4269d1be1e07ce6eceae189e1a176b35e578c954
Bryan Ramos <bryan.ramos@hey.com>
2022-05-05 06:39:02 -0400

    small refactor, add support for 'last' inbuilt function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -4,7 +4,21 @@ import "monkey/object"
 
 var builtins = map[string]*object.Builtin{
 	//len()
-	"len": &object.Builtin{Fn: __len__},
+	"len":   {Fn: __len__},
+	"first": {Fn: __first__},
+	"last":  {Fn: __last__},
+}
+
+func checkForArrayErrors(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments, got %d wanted 1", len(args))
+	}
+
+	if !isArray(args[0]) {
+		return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+	}
+
+	return NULL
 }
 
 func __len__(args ...object.Object) object.Object {
@@ -24,4 +38,30 @@ func __len__(args ...object.Object) object.Object {
 	default:
 		return newError("argument to `len` not supported, got %s", args[0].Type())
 	}
+}
+
+func __first__(args ...object.Object) object.Object {
+	checkForArrayErrors(args...)
+
+	arr := args[0].(*object.Array)
+
+	if len(arr.Elements) > 0 {
+		return arr.Elements[0]
+	}
+
+	return NULL
+}
+
+func __last__(args ...object.Object) object.Object {
+	checkForArrayErrors(args...)
+
+	arr := args[0].(*object.Array)
+
+	length := len(arr.Elements)
+	if length > 0 {
+		return arr.Elements[length-1]
+	}
+
+	return NULL
+
 }
