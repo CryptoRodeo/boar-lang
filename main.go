def5469879ad72ad43cc004c7afcd9fd37239226
Bryan Ramos <bryan.ramos@hey.com>
2022-05-28 21:11:00 -0400

    update start prompt message

--- main.go
+++ main.go
@@ -14,6 +14,6 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Hello %s, (use Ctrl+C to exit)\n", user.Username)
+	fmt.Printf("Hello %s, (use Ctrl+C or type 'exit()' to exit)\n", user.Username)
 	repl.Start(os.Stdin, os.Stdout)
 }
