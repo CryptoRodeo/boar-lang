09f6835510a8bfb12cebfe46d4b0755eb8490fce
Kilgore Trout <b.r.tech95@gmail.com>
2022-05-29 18:26:29 -0400

    refactor main to handle interpreter prompt and reading from files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,44 +11,39 @@ import (
 	"monkey/object"
 	"monkey/parser"
 	"monkey/repl"
+	"monkey/setuphelpers"
 	"os"
 	"os/user"
+	"path/filepath"
 	"strings"
 
 	"github.com/TwiN/go-color"
 )
 
 func main() {
-	user, err := user.Current()
-
-	if err != nil {
-		panic(err)
+	// no arguments passed
+	if len(os.Args) == 1 {
+		printHelpMenu()
+		return
 	}
 
-	ctrlC := color.Ize(color.Red, "Ctrl+C")
-	terminator := color.Ize(color.Red, "exit()")
-	userName := color.Ize(color.Cyan, user.Username)
-	fmt.Printf("Hello %s, use (%s or type '%s' to exit)\n", userName, ctrlC, terminator)
-
 	switch os.Args[1] {
-
 	case "--prompt":
+		printInterpreterPrompt()
 		repl.Start(os.Stdin, os.Stdout)
+	case "-f":
+		evaluateFile(os.Stdin, os.Stdout, os.Args[2])
 	default:
-		EvaluateFile(os.Stdin, os.Stdout, os.Args[1])
+		printHelpMenu()
 	}
 }
 
-func EvaluateFile(in io.Reader, out io.Writer, fileName string) {
+func evaluateFile(in io.Reader, out io.Writer, filePath string) {
 	env := object.NewEnvironment()
-	repl.LoadBuiltInMethods(env)
-	content, err := ioutil.ReadFile(fileName)
+	setuphelpers.LoadBuiltInMethods(env)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, code := range strings.Split(string(content), "\n") {
+	fileContent := locateFile(filePath)
+	for _, code := range fileContent {
 		// pass it through the lexer
 		l := lexer.New(code)
 		// pass lexer generated tokens to the parser
@@ -56,7 +52,7 @@ func EvaluateFile(in io.Reader, out io.Writer, fileName string) {
 		program := p.ParseProgram()
 
 		if len(p.Errors()) != 0 {
-			repl.PrintParserErrors(out, p.Errors())
+			setuphelpers.PrintParserErrors(out, p.Errors())
 			continue
 		}
 
@@ -70,4 +66,62 @@ func EvaluateFile(in io.Reader, out io.Writer, fileName string) {
 		}
 
 	}
+}
+
+func locateFile(filePath string) []string {
+	return formatUserFilePathInput(filePath)
+}
+
+func formatUserFilePathInput(filePath string) []string {
+	var errorFound error
+	var fileContents []byte
+	var fullFilePath string
+	pwd, _ := os.Getwd()
+
+	// ./fileName.mke
+	if filePath[0] == '.' && filePath[1] == '/' {
+		fullFilePath = filePath
+	} else {
+		fullFilePath = pwd + "/" + filePath
+	}
+
+	fileExtension, validFile := validateFileExtension(fullFilePath)
+
+	if !validFile {
+		log.Fatalf("Invalid file type passed, expected a .mke file, got %s instead", fileExtension)
+	}
+
+	fileContents, errorFound = ioutil.ReadFile(fullFilePath)
+	if errorFound != nil {
+		log.Fatal(errorFound)
+	}
+
+	// Split by ';'
+	res := strings.Split(string(fileContents), ";")
+
+	return res
+}
+
+func validateFileExtension(fPath string) (string, bool) {
+	fileExtension := filepath.Ext(fPath)
+	return fileExtension, fileExtension == ".mke"
+}
+func printHelpMenu() {
+	var out bytes.Buffer
+	out.WriteString("--prompt to use the interpreter\n")
+	out.WriteString("-f FILE to evaluate a .mke file\n")
+	fmt.Println(out.String())
+}
+
+func printInterpreterPrompt() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	ctrlC := color.Ize(color.Red, "Ctrl+C")
+	terminator := color.Ize(color.Red, "exit()")
+	userName := color.Ize(color.Cyan, user.Username)
+	fmt.Printf("Hello %s, use (%s or type '%s' to exit)\n", userName, ctrlC, terminator)
 }
