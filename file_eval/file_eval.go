e1a78060a8c4c923dce8fc84a1368e5976d244a5
Bryan Ramos <bryan.ramos@hey.com>
2022-06-02 05:54:19 -0400

    create file_eval package, evlauate files here

---
+++ file_eval/file_eval.go
@@ -0,0 +1,81 @@
+package file_eval
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+	"monkey/setuphelpers"
+	"os"
+	"path/filepath"
+)
+
+func EvaluateFile(in io.Reader, out io.Writer, filePath string) {
+	env := object.NewEnvironment()
+	setuphelpers.LoadBuiltInMethods(env)
+
+	fileContent := locateFile(filePath)
+	// pass it through the lexer
+	l := lexer.New(fileContent)
+	// pass lexer generated tokens to the parser
+	p := parser.New(l)
+	// parse the program
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		setuphelpers.PrintParserErrors(out, p.Errors())
+		return
+	}
+
+	//print the currently evaluated program
+	evaluated := evaluator.Eval(program, env)
+	if evaluated != nil {
+		// apply syntax highlighting
+		io.WriteString(out, evaluated.Inspect())
+		io.WriteString(out, "\n")
+	}
+
+}
+
+func locateFile(filePath string) string {
+	path := formatUserFilePathInput(filePath)
+	return findFile(path)
+}
+
+func formatUserFilePathInput(filePath string) string {
+	var fullFilePath string
+	pwd, _ := os.Getwd()
+
+	// ./fileName.mk
+	if filePath[0] == '.' && filePath[1] == '/' {
+		fullFilePath = filePath
+	} else {
+		fullFilePath = pwd + "/" + filePath
+	}
+
+	fileExtension, validFile := validateFileExtension(fullFilePath)
+
+	if !validFile {
+		log.Fatalf("Invalid file type passed, expected a .mk file, got %s instead", fileExtension)
+	}
+
+	return fullFilePath
+}
+
+func findFile(filePath string) string {
+	file, err := ioutil.ReadFile(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(file)
+}
+
+func validateFileExtension(fPath string) (string, bool) {
+	fileExtension := filepath.Ext(fPath)
+	return fileExtension, fileExtension == ".mk"
+}
